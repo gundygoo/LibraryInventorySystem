15c8d39257d5c3e8053dfb1605ac74f18e93eb52
ScarlettHawke <katee.lyn@gmail.com>
2016-10-26 21:33:20 -0500

    Added screens
    user options is the page after employee clicks "user accounts button on
    welcome screen" this will have two buttons, add user and edit user

---
+++ app/src/main/java/com/dreamteam/softwareengineering/libraryinventorysystem/EmployeeAddUser.java
@@ -0,0 +1,13 @@
+package com.dreamteam.softwareengineering.libraryinventorysystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeAddUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_add_user);
+    }
+}
