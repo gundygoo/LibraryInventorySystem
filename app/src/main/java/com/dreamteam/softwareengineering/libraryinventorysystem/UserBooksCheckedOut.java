f4a44b877c146da6513e67d9d527604a4488d962
ScarlettHawke <katee.lyn@gmail.com>
2016-10-27 00:07:04 -0500

    more shtuff

---
+++ app/src/main/java/com/dreamteam/softwareengineering/libraryinventorysystem/UserBooksCheckedOut.java
@@ -0,0 +1,13 @@
+package com.dreamteam.softwareengineering.libraryinventorysystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserBooksCheckedOut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_books_checked_out);
+    }
+}
