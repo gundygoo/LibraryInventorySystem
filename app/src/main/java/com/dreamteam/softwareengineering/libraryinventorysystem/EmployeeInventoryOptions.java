a9300e4dd1b87fd2fc0bcc1c932c985df23cf0a3
ScarlettHawke <katee.lyn@gmail.com>
2016-10-26 21:42:13 -0500

    MOAR SCREENS
    ALL THE SCREENS

---
+++ app/src/main/java/com/dreamteam/softwareengineering/libraryinventorysystem/EmployeeInventoryOptions.java
@@ -0,0 +1,13 @@
+package com.dreamteam.softwareengineering.libraryinventorysystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeInventoryOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_inventory_options);
+    }
+}
