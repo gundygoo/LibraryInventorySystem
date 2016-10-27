9d0d491c79ab70dbd01c451e1b880eeb47b18267
KollmansbergerK <kollmansbergerk0160@my.uwstout.edu>
2016-10-26 21:18:39 -0500

    Laid the foundations for welcome and check out of the employee as well as the login screen

--- app/src/main/java/com/dreamteam/softwareengineering/libraryinventorysystem/EmployeeCheckOut.java
+++ app/src/main/java/com/dreamteam/softwareengineering/libraryinventorysystem/EmployeeCheckOut.java
@@ -1,13 +1,44 @@
 package com.dreamteam.softwareengineering.libraryinventorysystem;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class EmployeeCheckOut extends AppCompatActivity {
+    public EditText bookNameTextBox;
+    public EditText customerUserNameTextBox;
+    public Button checkOut;
+    public Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_check_out);
+        context = getApplicationContext();
+        bookNameTextBox = (EditText) findViewById(R.id.whateverKateeCallsItHere);
+        final String bookNameText = bookNameTextBox.getText().toString();
+        customerUserNameTextBox = (EditText) findViewById(R.id.whateverKateeCallsItHere);
+        final String customerUsernameText = customerUserNameTextBox.getText().toString();
+        checkOut = (Button) findViewById(R.id.idHere);
+        checkOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    DatabaseAccessor.CheckoutBookToCustomer(bookNameText, customerUsernameText, context);
+                    Intent employeeWelcome = new Intent(context, EmployeeWelcome.class);
+                    startActivity(employeeWelcome);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(context, "Error checking out book to customer", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/dreamteam/softwareengineering/libraryinventorysystem/EmployeeWelcome.java
+++ app/src/main/java/com/dreamteam/softwareengineering/libraryinventorysystem/EmployeeWelcome.java
@@ -1,13 +1,57 @@
 package com.dreamteam.softwareengineering.libraryinventorysystem;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EmployeeWelcome extends AppCompatActivity {
 
+    public Button CheckInButton;
+    public Button CheckOutButton;
+    public Button InventoryButton;
+    public Button UsersButton;
+    public Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        context = getApplicationContext();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_welcome);
+        CheckInButton = (Button) findViewById(R.id.idHere);
+        CheckInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent employeeCheckIn= new Intent(context, EmployeeCheckIn.class);
+                startActivity(employeeCheckIn);
+            }
+        });
+        CheckOutButton = (Button) findViewById(R.id.idHere);
+        CheckOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent employeeCheckOut= new Intent(context, EmployeeCheckOut.class);
+                startActivity(employeeCheckOut);
+            }
+        });
+        InventoryButton = (Button) findViewById(R.id.idHere);
+        CheckInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent employeeInventory= new Intent(context, EmployeeInventory.class);
+                startActivity(employeeInventory);
+            }
+        });
+        UsersButton = (Button) findViewById(R.id.idHere);
+        UsersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent employeeUsers= new Intent(context, EmployeeUsers.class);
+                startActivity(employeeUsers);
+            }
+        });
     }
 }
